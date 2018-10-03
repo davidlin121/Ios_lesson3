50c8a28be4ddad2aa1d20ec14d88d0443521625a
menglong <meda4299@colorado.edu>
2018-10-02 18:32:12 -0600

    button action

---
+++ app_swoosh/app_swoosh/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  app_swoosh
+//
+//  Created by menglong dai on 9/27/18.
+//  Copyright © 2018 menglong dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var background: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
